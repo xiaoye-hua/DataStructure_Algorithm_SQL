9cbc1922b9a987bc48149c75ee086e4d84b5f048
xiaoye-hua <huag@kth.se>
2020-10-15 07:45:58 +0800

    change 126

--- 126.py
+++ 126.py
@@ -12,8 +12,8 @@ from src.linked_list.LinkedList import LinkedList
 class Solution:
     def findLadders(self, beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
         """
-        First try: DFS --> ETL
-        Second try: BFS --> ETL
+        First try: BFS --> don't know how to store the path
+        Second try: BFS+DFS --> BFS for the length; DFS for all sequence  --> ETL
         :param beginWord:
         :param endWord:
         :param wordList:
@@ -27,41 +27,51 @@ class Solution:
                 if self.check_similar(wordList[begin_idx], wordList[end_idx]):
                     matrix[wordList[begin_idx]].append(wordList[end_idx])
                     matrix[wordList[end_idx]].append(wordList[begin_idx])
-        # self.matrix = matrix
-        # # print(self.matrix)
-        # self.res = float("inf")
-        # self.target = endWord
+
+        self.matrix = matrix
+        self.res = []
+        self.target = endWord
+        self.length = self.bfs(beginWord)
+        # return self.length
+        if self.length == 0:
+            return self.res
+        else:
+            self.dfs(beginWord, 1, [], [])
+            return self.res
+
+    def bfs(self, beginWord):
         queue = [beginWord]
         depth = 0
         visited = []
         found = False
-        res = []
+        res = 0
         while len(queue) != 0 and not found:
             depth += 1
             num = len(queue)
             for _ in range(num):
                 node = queue.pop(0)
-                if node == endWord:
-                    found = True
+                if node == self.target:
                     return depth
                 else:
                     if node not in visited:
-                        queue += matrix[node]
+                        queue += self.matrix[node]
                         visited.append(node)
         return res
 
-    #     self.dfs(beginWord, 1, [])
-    #     return 0 if self.res==float("inf") else self.res
-    #
-    # def dfs(self, node, depth, passed):
-    #     new_passed = passed.copy()
-    #     new_passed.append(node)
-    #     for child in self.matrix[node]:
-    #         if child not in new_passed:
-    #             if child == self.target:
-    #                 self.res = min(self.res, depth+1)
-    #             else:
-    #                 self.dfs(child, depth+1, new_passed)
+    def dfs(self, node, depth, passed, path):
+        new_passed = passed.copy()
+        new_passed.append(node)
+        new_path = path.copy()
+        new_path.append(node)
+        if depth > self.length:
+            return
+        elif depth == self.length:
+            if node == self.target:
+                self.res.append(new_path)
+        else:
+            for child in self.matrix[node]:
+                if child not in new_passed:
+                    self.dfs(child, depth+1, new_passed, new_path)
     #
     def check_similar(self, a, b):
         diff_time = 0
@@ -79,5 +89,5 @@ wordList = ["hot","dot","dog","lot","log","cog"]
 # wordList = ["lest","leet","lose","code","lode","robe","lost"]
 
 # Output: 5
-res = Solution().ladderLength(beginWord, endWord, wordList)
+res = Solution().findLadders(beginWord, endWord, wordList)
 print(res)
